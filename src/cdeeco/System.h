bc80f018f9ddb40e63e547c33a7f1b6b98b93323
Vladimír Matěna <vlada.matena@gmail.com>
2014-05-07 18:19:49 +0200

    Draft fragment processing in System object

--- src/cdeeco/System.h
+++ src/cdeeco/System.h
@@ -10,7 +10,9 @@
 
 #include "KnowledgeCache.h"
 
-template<size_t CACHES>
+typedef uint32_t Timestamp;
+
+template<size_t CACHES, size_t REBROADCAST_SIZE>
 class System {
 public:
 	System() {
@@ -18,8 +20,25 @@ public:
 		memset(&caches, 0, sizeof(caches));
 	}
 
+	/** Broadcast knowledge fragment */
+	void broadcastFragment(KnowledgeFragment fragment) {
+		// TODO: Enqueue fragment for broadcasting
+	}
+
+	/** Process process received fragment */
+	void processFragment(KnowledgeFragment fragment) {
+		// TODO: Try to store fragment in one of the caches
+		// TODO: Store fragment in rebroadcast storage
+	}
+
 private:
 	KnowledgeCache *caches[CACHES];
+
+	struct ReboadcastRecord {
+		Timestamp recieved;
+		Timestamp rebroadcast;
+		KnowledgeFragment fragment;
+	} rebroadcast[REBROADCAST_SIZE];
 };
 
 #endif /* SYSTEM_H_ */
