a2ce37bb3510af114fc83395da4b3572fa3d3898
Vladimír Matěna <vlada.matena@gmail.com>
2014-05-21 23:15:26 +0200

    Draft Ensamble

---
+++ src/cdeeco/Ensamble.h
@@ -0,0 +1,63 @@
+/*
+ * Ensamble.h
+ *
+ *  Created on: 21. 5. 2014
+ *      Author: Vladimír Matěna
+ */
+
+#ifndef ENSAMBLE_H_
+#define ENSAMBLE_H_
+
+#include "Component.h"
+#include "KnowledgeCache.h"
+
+namespace CDEECO {
+
+	template <typename COORD_KNOWLEDGE, typename OUT_KNOWLEDGE,  typename MEMBER_KNOWLEDGE>
+	class Ensamble: FreeRTOSTask {
+	public:
+		Ensamble(Component<COORD_KNOWLEDGE> &coordinator, OUT_KNOWLEDGE &outKnowledge, KnowledgeCache<MEMBER_KNOWLEDGE> &cache, long period):
+			coordinator(coordinator), outKnowledge(outKnowledge), cache(cache), period(period) {};
+
+		virtual ~Ensamble() {
+		}
+
+		/**
+		 * Membership to function to be implemented
+		 *
+		 * @param coord Coordinator knowledge
+		 * @param member Member knowledge
+		 * @return Whenever the member is part of Ensamble
+		 */
+		virtual bool member(const COORD_KNOWLEDGE coord, const MEMBER_KNOWLEDGE member) = 0;
+
+		/**
+		 * Knowledge map function to be implemented
+		 *
+		 * @param coord Coordinator knowledge
+		 * @param member Member knowledge
+		 * @return Output knowledge for coordinator
+		 */
+		virtual OUT_KNOWLEDGE map(const COORD_KNOWLEDGE coord, const MEMBER_KNOWLEDGE member) = 0;
+
+	private:
+		long period;
+		Component<COORD_KNOWLEDGE> &coordinator;
+		OUT_KNOWLEDGE &outKnowledge;
+		KnowledgeCache<MEMBER_KNOWLEDGE> &cache;
+
+		/** Ensamble periodic task */
+		void run() {
+			// Schedule the task periodically
+			while (1) {
+				// TODO: For all knowledge from the cache check member and execute map
+
+				// Wait for next execution time
+				vTaskDelay(period / portTICK_PERIOD_MS);
+			}
+		}
+	};
+
+}
+
+#endif /* ENSAMBLE_H_ */
