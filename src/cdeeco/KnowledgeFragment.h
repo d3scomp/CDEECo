e8a6ce8f9beadba33a25a4155c19826b4e54e401
Vladimír Matěna <vlada.matena@gmail.com>
2014-07-05 22:37:32 +0200

    Document knowledge fragment

--- src/cdeeco/KnowledgeFragment.h
+++ src/cdeeco/KnowledgeFragment.h
@@ -5,8 +5,8 @@
  *      Author: Vladimír Matěna
  */
 
-#ifndef KNOWLEDGE_FRAGMENT_H_
-#define KNOWLEDGE_FRAGMENT_H_
+#ifndef KNOWLEDGE_FRAGMENT_H
+#define KNOWLEDGE_FRAGMENT_H
 
 #include <cstdint>
 #include <cstring>
@@ -14,23 +14,44 @@
 #include "Knowledge.h"
 
 namespace CDEECO {
+	/**
+	 * Knowledge fragment structure to be included directly in the packet
+	 */
 	struct KnowledgeFragment {
+		/// Maximum packet size
 		static const size_t MAX_PACKET_SIZE = 128;
+		/// Maximum size of the data part
 		static const size_t MAX_DATA_SIZE = 112;
+		/**
+		 * CDEECo++ magic value
+		 *
+		 * Used to identify the CDEECo++ packets
+		 */
 		static const uint32_t CDEECO_MAGIC = 0xcdeec044;
 
+		/// Knowledge type
 		Type type;
+		/// Knowledge component id
 		Id id;
+		/// Fragment size
 		size_t size;
+		/// Fragment offset in the knowledge
 		size_t offset;
+		/// Fragment data
 		char data[MAX_DATA_SIZE];
 
+		/**
+		 * Get used length of the packet
+		 *
+		 * @return Size of used packet part
+		 */
 		size_t length() const {
 			return sizeof(KnowledgeFragment) - MAX_DATA_SIZE + size;
 		}
 	};
 
+	// Ensure knowledge fragment fits into a packet
 	static_assert(KnowledgeFragment::MAX_PACKET_SIZE == sizeof(KnowledgeFragment), "Knowledge fragment size not equal to packet size.");
 }
 
-#endif // KNOWLEDGE_FRAGMENT_H_
+#endif // KNOWLEDGE_FRAGMENT_H
